e44218c4275d842f34dd21da01ee05cd544cd3cb
= <=>
2021-12-28 08:33:48 +0700

    uas-sister

---
+++ php/src/code crud/config.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = 'localhost';
+    $user = 'admin';
+    $pass = 'Admin123';
+    $database = 'crud_siswa';
+    $connect = mysqli_connect($host, $user, $pass, $database);
+    
+?>
